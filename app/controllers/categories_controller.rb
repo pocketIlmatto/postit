ecc35efb46ad78ff8ecdac16c71bd75e59c586ec
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2014-11-24 21:53:33 -0800

    Adding category form, controllers, routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+  
+  def show
+    @posts = @category.posts
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Your category was created"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def format_url(url)
+    url = /^http/i.match(url) ? url : "http://#{url}"
+  end
 end
