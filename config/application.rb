9c3a74f9d40509882a7bc69fb3e16c0e8c4ca2fa
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2014-11-29 14:01:07 -0800

    Updating environment variables

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module PostitTemplate
 
     # Tealeaf note: Bootstrap sass gem addition
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
