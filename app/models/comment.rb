17d9e537365e40467e1ac6aee3a9450e50a86231
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2014-12-08 12:37:13 -0800

    Moving repeated vote business logic to a module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,20 +1,9 @@
 class Comment < ActiveRecord::Base
-	belongs_to :creator, class_name: "User", foreign_key: :user_id
+	include Voteable
+  belongs_to :creator, class_name: "User", foreign_key: :user_id
   belongs_to :post
   validates  :body, presence: true
   validates :user_id, presence: true
   validates :post_id, presence: true
-  has_many :votes, as: :voteable
-
-  def total_votes
-    upvotes - downvotes
-  end
-
-  def upvotes
-    self.votes.where(vote: true).size
-  end
-
-  def downvotes
-    self.votes.where(vote: false).size
-  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,15 @@
 include ApplicationHelper
 class Post < ActiveRecord::Base
-	belongs_to :creator, class_name: "User", foreign_key: :user_id
+  include Voteable
+
+  belongs_to :creator, class_name: "User", foreign_key: :user_id
 	has_many :comments, counter_cache: true
 	has_and_belongs_to_many :categories
 	validates :user_id, presence: true
 	validates :title, presence: true
 	validates :url, presence: true
-  has_many :votes, as: :voteable
   before_save :generate_slug
-	
-  def total_votes
-    upvotes - downvotes
-  end
-
-  def upvotes
-    self.votes.where(vote: true).size
-  end
-
-  def downvotes
-    self.votes.where(vote: false).size
-  end
-
+  	
   def to_param
     slug
   end
